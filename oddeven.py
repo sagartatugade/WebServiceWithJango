51c149fc82c0505d5aefafa8f18d5e580e4283a1
Admin <sagartatugade@gmail.com>
2019-10-05 12:35:58 +0530

    New changes

---
+++ oddeven.py
@@ -0,0 +1,30 @@
+
+
+
+
+def Oddeven():
+    list = [3,5,7,13,45,56,67,89]
+
+    for i in list:
+        if list.index(i)%2==0:
+            print(i)
+
+        else:
+             print(i*2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Oddeven()
+
